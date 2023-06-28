e564f0350ae8d0863c2d53b796cf9f1d241d19fe
Kuro1Fury <skycoder0820@gmail.com>
2023-06-28 12:54:56 +0800

    init

---
+++ src/main/java/com/example/SimpleManagementSystembackend/SimpleManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleManagementSystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleManagementSystemBackendApplication.class, args);
+	}
+
+}
